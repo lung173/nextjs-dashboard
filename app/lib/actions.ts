aeccb219b9201be767bcb28b038600b70675c222
lung173 <l200230128@student.ums.ac.id>
2025-12-20 17:43:54 +0700

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -124,19 +124,16 @@ export async function authenticate(
 
   try {
     await signIn('credentials', {
-      ...Object.fromEntries(formData),
-      redirect: false,
+      email: formData.get('email'),
+      password: formData.get('password'),
+      redirectTo, // 🔥 INI KUNCINYA
     });
-
-    redirect(redirectTo);
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
+      if (error.type === 'CredentialsSignin') {
+        return 'Invalid credentials.';
       }
+      return 'Something went wrong.';
     }
     throw error;
   }
