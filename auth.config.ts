bfee28af5ec5b4ad33521375e08a75685df2a09c
lung173 <l200230128@student.ums.ac.id>
2025-12-20 17:51:54 +0700

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -5,29 +5,7 @@ export const authConfig = {
     signIn: '/login',
   },
 
-  callbacks: {
-
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const pathname = nextUrl.pathname;
-
-
-      if (pathname.startsWith('/login')) {
-        return true;
-      }
-
-      if (pathname.startsWith('/api')) {
-        return true;
-      }
-
-      if (pathname.startsWith('/dashboard')) {
-        return isLoggedIn;
-      }
-
-      return true;
-    },
-  },
+  callbacks: {},
 
   providers: [],
 } satisfies NextAuthConfig;
-
